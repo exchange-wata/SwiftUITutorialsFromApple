274e7f670b1cc4fffece194f86ab78d9580e86f1
exchange-wata <exchange.wata0907@gmail.com>
2024-04-16 14:16:22 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 渡辺瑞樹 on 2024/04/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
